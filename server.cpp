9485b5190263d38525de74ce0dd98200d1141bb4
Daniel Jimenez Graindorge <danjimen@c1r9s2.42madrid.com>
2025-06-20 13:26:29 +0200

    Fixed private member names and intentation

--- server.cpp
+++ server.cpp
@@ -1,25 +1,27 @@
 #include "server.hpp"
 
-Server::Server(int port) : port(port), server_fd(-1) {}
+Server::Server(int port) : _port(port), _server_fd(-1) {}
 
 Server::~Server()
 {
-	if (server_fd != -1)
-		close(server_fd);
+	if (_server_fd != -1)
+		close(_server_fd);
 }
 
 void Server::start()
 {
 	struct sockaddr_in address;
 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
+	_server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (_server_fd == -1)
+	{
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 
 	int opt = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+	if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+	{
 		perror("setsockopt");
 		exit(EXIT_FAILURE);
 	}
@@ -27,17 +29,19 @@ void Server::start()
 	std::memset(&address, 0, sizeof(address));
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(port);
+	address.sin_port = htons(_port);
 
-	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+	if (bind(_server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+	{
 		perror("bind");
 		exit(EXIT_FAILURE);
 	}
 
-	if (listen(server_fd, 10) < 0) {
+	if (listen(_server_fd, 10) < 0)
+	{
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 
-	std::cout << "Servidor escuchando en el puerto " << port << std::endl;
+	std::cout << "Servidor escuchando en el puerto " << _port << std::endl;
 }
