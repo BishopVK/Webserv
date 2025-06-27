8d6886b12484cdb60bec7ff31d0687a7f606c67f
Daniel Jimenez Graindorge <danjimen@c1r9s2.42madrid.com>
2025-06-27 10:01:56 +0200

    Added handmaded server object

--- main.cpp
+++ main.cpp
@@ -6,12 +6,56 @@
 /*   By: danjimen,isainz-r,serferna <webserv@stu    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 12:44:17 by danjimen,is       #+#    #+#             */
-/*   Updated: 2025/06/26 14:01:48 by danjimen,is      ###   ########.fr       */
+/*   Updated: 2025/06/27 10:00:10 by danjimen,is      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/Config.hpp"
 #include "include/Server.hpp"
+
+Server	create_server_for_testing()
+{
+	Server server;
+	server.setRoot("www.testing.cat");
+	server.addIndexFile("index.html index.htm;");
+	server.setAutoindex(true);
+	server.setClientMaxBodySize(10);
+	server.addErrorPage(404, "/errors/404.html");
+	server.setIp("127.0.0.2");
+	server.addPort(1234);
+	server.setServerName("Testing Server in Catalán");
+
+	Location location1;
+	location1.setRoute("/var/www/html/location1Route");
+	location1.setRoot("/var/www/html/location1Root");
+	location1.addIndexFile("testing.html");
+	location1.setAutoindex(false);
+	location1.addErrorPage(505, "/errors/50x.html");
+	location1.setClientMaxBodySize(5);
+	location1.addCgi(".php", "/usr/bin/php-cgi");
+	location1.setReturnData(301, "http://example.com");
+	location1.addMethod("GET");
+	location1.addMethod("POST");
+	
+	server.addLocation("/testing/", location1);
+
+	Location location2;
+	location2.setRoute("/var/www/html/location2Route");
+	location2.setRoot("/var/www/html/location2Root");
+	location2.addIndexFile("location.html");
+	location2.setAutoindex(true);
+	location2.addErrorPage(400, "/errors/40x.html");
+	location2.setClientMaxBodySize(20);
+	location2.addCgi(".py", "/usr/bin/python3");
+	location2.setReturnData(302, "http://python-example.com");
+	location2.addMethod("DELETE");
+	
+	server.addLocation("/location/", location2);
+
+	//server.print();
+
+	return server;
+}
 
 int main(int argc, char** argv)
 {
@@ -41,8 +85,10 @@ int main(int argc, char** argv)
 
 	// Aquí más adelante vendrá el parser
 
-	int port = 8080; // lo obtendremos del parser cuando lo hagamos
-	Server server(port);
+	//int port = 8080; // lo obtendremos del parser cuando lo hagamos
+	//Server server(port);
+	Server server = create_server_for_testing(); // TESTING
+	server.print();
 	server.start();
 	while (true) // Mantener el servidor corriendo
 		;
