dfd0f52e81a13ea4190892dbd5d597d0820ecd2d
Daniel Jimenez Graindorge <danjimen@c1r9s4.42madrid.com>
2025-07-14 12:56:48 +0200

    Upload web page created

---
+++ var/www/html/upload/upload.php
@@ -0,0 +1,15 @@
+<?php
+echo "Content-type: text/html\n\n";
+if (isset($_FILES['file'])) {
+    $tmp = $_FILES['file']['tmp_name'];
+    $name = basename($_FILES['file']['name']);
+    $destination = "./uploads/" . $name;
+    if (move_uploaded_file($tmp, $destination)) {
+        echo ":marca_de_verificación_blanca: File uploaded successfully to $destination";
+    } else {
+        echo ":x: Failed to move uploaded file.";
+    }
+} else {
+    echo ":advertencia: No file received.";
+}
+?>
