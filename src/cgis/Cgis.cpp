a546445afdde8b76003702674c427b1ab87b0c5b
BishopVK <danielgraindorge@gmail.com>
2025-07-17 13:09:45 +0200

    Added logger

--- src/cgis/Cgis.cpp
+++ src/cgis/Cgis.cpp
@@ -170,7 +170,14 @@ Cgis::Cgis( std::string method, std::string file_path, std::string file_name,
 	// 	this->body = deschunk(body);
 	// }
 	this->chunked = chunked;
-	Logger::instance().debug(">> BODY: " + body);
+	Logger::instance().debug("CONSTRUCTOR ==> method: " + method);
+	Logger::instance().debug("CONSTRUCTOR ==> file_path: " + file_path);
+	Logger::instance().debug("CONSTRUCTOR ==> file_name: " + file_name);
+	Logger::instance().debug("CONSTRUCTOR ==> content_type: " + content_type);
+	Logger::instance().debug("CONSTRUCTOR ==> boundary: " + boundary);
+	Logger::instance().debug("CONSTRUCTOR ==> content_lenght: " + content_lenght);
+	Logger::instance().debug("CONSTRUCTOR ==> body: " + body);
+	Logger::instance().debug("CONSTRUCTOR ==> chunked: " + chunked);
 }
 
 Cgis::~Cgis()
