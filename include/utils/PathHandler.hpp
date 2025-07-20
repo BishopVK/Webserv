54678d9822952469444289384986bad69dead844
BishopVK <danielgraindorge@gmail.com>
2025-07-20 23:56:23 +0200

    Removed commented methods. Added message when uploads folder its empty

--- include/utils/PathHandler.hpp
+++ include/utils/PathHandler.hpp
@@ -18,16 +18,6 @@ class PathHandler
 
     static bool        isPrefixMatch(const std::string& requestPath, const std::string& locationPath);
     static std::string getRelativePath(const std::string& requestPath, const std::string& locationPath);
-
-    // ADDED BY DANI
-    // Parsea un body "a=1&b=2" en un map<clave,valor>
-    static std::map<std::string, std::string> parseUrlEncoded(const std::string& body);
-
-    // Decodifica %XX y + → espacio
-    static std::string urlDecode(const std::string& str);
-
-    // Obtiene sólo el nombre de fichero de una ruta
-    static std::string getBasename(const std::string& path);
 };
 
 #endif
